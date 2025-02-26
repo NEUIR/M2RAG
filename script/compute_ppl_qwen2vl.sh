62893e01f3d4465b3197798220d700b3d2583641
namingsohard <1806880383@qq.com>
2025-02-26 16:46:29 +0800

    update

--- script/compute_ppl_qwen2vl.sh
+++ script/compute_ppl_qwen2vl.sh
@@ -3,8 +3,8 @@ export CUDA_VISIBLE_DEVICES=0
 python ../src/qwen_vl/compute_ppl_qwen2vl.py \
 --model_path path/to/Qwen2-VL-7B-Instruct \
 --data_path ../data/m2rag/image_rerank/test_data.jsonl \
---trec_path path/to/retrieve_results.trec \
---image_path path/to/images \
+--trec_path ../output/retrieval/image_rerank/webqa_image_rerank_query_test_visualbge_5_image.trec \
+--image_path ../output/retrieval/image_rerank/webqa_image_rerank_test_retrieval_images_5 \
 --out_path ../output/test_qwen2vl_5_ppl.json \
 --topk 5
 
@@ -13,7 +13,7 @@ export CUDA_VISIBLE_DEVICES=0
 python ../src/qwen_vl/compute_ppl_qwen2vl.py \
 --model_path path/to/fine_tuned_model \
 --data_path ../data/m2rag/image_rerank/test_data.jsonl \
---trec_path path/to/retrieve_results.trec \
---image_path path/to/images \
+--trec_path ../output/retrieval/image_rerank/webqa_image_rerank_query_test_visualbge_5_image.trec \
+--image_path ../output/retrieval/image_rerank/webqa_image_rerank_test_retrieval_images_5 \
 --out_path ../output/test_sft_qwen2vl_5_ppl.json \
 --topk 5

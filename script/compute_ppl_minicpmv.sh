d4223f3caffe411c8c8735b786253a3b6fcf0170
namingsohard <1806880383@qq.com>
2025-03-14 19:52:26 +0800

    update

--- script/compute_ppl_minicpmv.sh
+++ script/compute_ppl_minicpmv.sh
@@ -5,8 +5,18 @@ python ../src/minicpmv/compute_ppl_minicpmv.py \
 --data_path ../data/m2rag/image_rerank/test_data.jsonl \
 --trec_path ../output/retrieval/image_rerank/webqa_image_rerank_query_test_visualbge_5_image.trec \
 --image_path ../output/retrieval/image_rerank/webqa_image_rerank_test_retrieval_images_5 \
---out_path ../output/image_rerank/test_minicpmv_ppl.json \
+--out_path ../output/image_rerank/test_minicpmv_5_ppl.json \
 --topk 5
+
+python ../src/select_lowest_ppl_data.py \
+--input_file ../output/image_rerank/test_minicpmv_5_ppl.json \
+--output_file ../output/image_rerank/test_minicpmv_5_ppl_selected.jsonl \
+--output_dir ../output/image_rerank/image_rerank_retrieve_minicpmv_ppl_top \
+--restore_image \
+--topk 5
+
+# after restored images, you can use pytorch_fid to calculate the metric between the output_dir and the ground truth images dir
+
 
 # for fine-tuned minicpmv
 export CUDA_VISIBLE_DEVICES=0
@@ -16,5 +26,14 @@ python ../src/minicpmv/compute_ppl_minicpmv.py \
 --data_path ../data/m2rag/image_rerank/test_data.jsonl \
 --trec_path ../output/retrieval/image_rerank/webqa_image_rerank_query_test_visualbge_5_image.trec \
 --image_path ../output/retrieval/image_rerank/webqa_image_rerank_test_retrieval_images_5 \
---out_path ../output/image_rerank/test_sft_minicpmv_ppl.json \
+--out_path ../output/image_rerank/test_sft_minicpmv_5_ppl.json \
 --topk 5
+
+python ../src/select_lowest_ppl_data.py \
+--input_file ../output/image_rerank/test_sft_minicpmv_5_ppl.json \
+--output_file ../output/image_rerank/test_sft_minicpmv_5_ppl_selected.jsonl \
+--output_dir ../output/image_rerank/image_rerank_retrieve_sft_minicpmv_ppl_top \
+--restore_image \
+--topk 5
+
+# after restored images, you can use pytorch_fid to calculate the metric between the output_dir and the ground truth images dir
--- script/compute_ppl_qwen2vl.sh
+++ script/compute_ppl_qwen2vl.sh
@@ -8,6 +8,17 @@ python ../src/qwen_vl/compute_ppl_qwen2vl.py \
 --out_path ../output/test_qwen2vl_5_ppl.json \
 --topk 5
 
+python ../src/select_lowest_ppl_data.py \
+--input_file ../output/image_rerank/test_qwen2vl_5_ppl.json \
+--output_file ../output/image_rerank/test_qwen2vl_5_ppl_selected.jsonl \
+--output_dir ../output/image_rerank/image_rerank_retrieve_qwen2vl_ppl_top \
+--restore_image \
+--topk 5
+
+# after restored images, you can use pytorch_fid to calculate the metric between the output_dir and the ground truth images dir
+
+
+
 # for fine-tuned Qwen2VL
 export CUDA_VISIBLE_DEVICES=0
 python ../src/qwen_vl/compute_ppl_qwen2vl.py \
@@ -17,3 +28,12 @@ python ../src/qwen_vl/compute_ppl_qwen2vl.py \
 --image_path ../output/retrieval/image_rerank/webqa_image_rerank_test_retrieval_images_5 \
 --out_path ../output/test_sft_qwen2vl_5_ppl.json \
 --topk 5
+
+python ../src/select_lowest_ppl_data.py \
+--input_file ../output/image_rerank/test_sft_qwen2vl_5_ppl.json \
+--output_file ../output/image_rerank/test_sft_qwen2vl_5_ppl_selected.jsonl \
+--output_dir ../output/image_rerank/image_rerank_retrieve_sft_qwen2vl_ppl_top \
+--restore_image \
+--topk 5
+
+# after restored images, you can use pytorch_fid to calculate the metric between the output_dir and the ground truth images dir
